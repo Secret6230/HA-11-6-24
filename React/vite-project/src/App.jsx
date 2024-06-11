fb8c7cd7f8a5aa963f6e413cc6e6e1a9ce5c8e6a
Secret6230 <akinkale054@hotmail.com>
2024-06-11 19:59:46 +0200

    HA-11-6-24

---
+++ React/vite-project/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import TodoList from "./TodoList";
+import "./Design.css"
+
+function App() {
+  return (
+    <>
+      <Header />
+      <TodoList />
+      <Footer />
+
+    </>
+  );
+}
+
+export default App;
---
+++ React/vite-project/src/Footer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Design.css"
+
+function Footer() {
+  return <div id="Footer">Footer</div>;
+}
+
+export default Footer;
